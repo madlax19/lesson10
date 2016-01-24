46774255585b8677e57378ab75aedcb5379edd0d
madlax19 <madlax19@mail.ru>
2016-01-24 21:26:36 +0200

    first task block animation

--- sample3/sample3/ViewController.m
+++ sample3/sample3/ViewController.m
@@ -10,45 +10,66 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *xconstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint;
 @property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidLayoutSubviews {
-	[super viewDidLayoutSubviews];
-}
-
-
 - (void)viewDidAppear:(BOOL)animated {
 	[super viewDidAppear:animated];
-	
-	for (NSLayoutConstraint *constraint in self.view.constraints) {
-		if (constraint.firstAttribute == NSLayoutAttributeCenterX) {
-			constraint.constant = 100.0f;
-			break;
-		}
-	}
-
-	[UIView animateWithDuration:4.4
-						  delay:0.0f
-						options:UIViewAnimationOptionAllowUserInteraction
-					 animations:^(void) {
-						 [self.view layoutIfNeeded];
-					 }
-					 completion:NULL];
-	
+    [self animateButtonFirst];
 }
 
-- (IBAction)buttonTapped:(id)sender {
-	NSLog(@"Tap");
+- (void)animateButtonFirst {
+    self.constraint.constant = self.view.bounds.size.width - self.button.bounds.size.width;
+    [UIView animateWithDuration:3.4f
+                          delay:0.0f
+                        options:UIViewAnimationOptionAllowUserInteraction
+                     animations:^(void) {
+                         [self.view layoutIfNeeded];
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.5 animations:^{
+                             self.button.transform = CGAffineTransformRotate(self.button.transform, M_PI);
+                         } completion:^(BOOL finished) {
+                             [self animateButtonSecond];
+                         }];
+                     }];
+}
+
+- (void)animateButtonSecond {
+    self.constraint.constant = 0;
+    [UIView animateWithDuration:3.4f
+                          delay:0.0f
+                        options:UIViewAnimationOptionAllowUserInteraction
+                     animations:^(void) {
+                         [self.view layoutIfNeeded];
+                     }
+                     completion:^(BOOL finished) {
+                         [UIView animateWithDuration:0.5 animations:^{
+                             self.button.transform = CGAffineTransformRotate(self.button.transform, M_PI);
+                         } completion:^(BOOL finished) {
+                             [self animateButtonFirst];
+                         }];
+                     }];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInView:self.view];
+    if ([self.button.layer.presentationLayer hitTest:touchLocation]) {
+        self.button.backgroundColor = [self randomColor];
+        NSLog(@"Tap");
+    }
+}
+
+- (UIColor*)randomColor {
+    return [UIColor colorWithRed:(CGFloat)(arc4random() % 256) / 255.f
+                           green:(CGFloat)(arc4random() % 255) / 255.f
+                            blue:(CGFloat)(arc4random() % 255) / 255.f
+                           alpha:1.0f];
 }
 
 @end
